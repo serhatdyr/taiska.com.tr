0dc1123406174a4c8e1e4266c2c296f8ba2aac8d
Enes Serbest <enesserbest@outlook.com>
2025-05-12 14:27:48 +0300

    Banner ve aboutus güncellendi.

--- components/VerticalSlider.js
+++ components/VerticalSlider.js
@@ -40,6 +40,24 @@ function VerticalSlider() {
             </div>
           </div>
         </div>
+        <div className="slick-slide-item">
+          <Image src="/src/assets/images/slider/03.jpg" width="1920" height="1080" />
+          <div className="slider-text">
+            <div className="slider-text-sub">
+              <div className="slider-text-first">interior</div>
+              <div className="slider-text-second">design</div>
+            </div>
+          </div>
+        </div>
+        <div className="slick-slide-item">
+          <Image src="/src/assets/images/slider/04.jpg" width="1920" height="1080" />
+          <div className="slider-text">
+            <div className="slider-text-sub">
+              <div className="slider-text-first">steel</div>
+              <div className="slider-text-second">works</div>
+            </div>
+          </div>
+        </div>
       </Slider>
     </div>
   );
